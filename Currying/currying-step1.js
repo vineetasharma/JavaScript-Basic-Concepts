e0d93f3ef0880fc9b9bb34c1813d0321282f0fd6
Vineeta Sharma <vineetasharma18791@gmail.com>
2022-06-02 23:51:55 +0530

    Currying uses

--- Currying/currying-step1.js
+++ Currying/currying-step1.js
@@ -4,23 +4,41 @@
  * Currying doesn’t call a function. It just transforms it.
  */
 
- function curry(f) { // curry(f) does the currying transform
-    return function(a) {
-      return function(b) {
-        return f(a, b);
-      };
+function curry(f) { // curry(f) does the currying transform
+  return function (a) {
+    return function (b) {
+      return f(a, b);
     };
+  };
+}
+
+// usage
+function sum(a, b) {
+  return a + b;
+}
+
+let curriedSum = curry(sum);
+
+console.log(curriedSum(1)(2)); // 3
+console.log(curriedSum(1)(2)(3)); // TypeError: curriedSum(...)(...) is not a function
+/**
+ * In the above console.log we are trying to pass 3 arguments but it can accept 2 args only.
+ */
+
+
+/**
+ * Currying & Partial application
+ * Some might start to think that the number of nested functions a curried function has depends on the number of arguments it receives. Yes, that makes it a curry
+ */
+
+
+function sumSimple(a) {
+  return (b, c) => {
+    return a * b * c
   }
-  
-  // usage
-  function sum(a, b) {
-    return a + b;
-  }
-  
-  let curriedSum = curry(sum);
-  
-  console.log( curriedSum(1)(2) ); // 3
-  console.log(curriedSum(1)(2)(3)); // TypeError: curriedSum(...)(...) is not a function
-  /**
-   * In the above console.log we are trying to pass 3 arguments but it can accept 2 args only.
-   */
+}
+
+let x = sumSimple(10);
+console.log(x(3, 12)); //360
+console.log(x(20, 12)); // 2400
+console.log(x(20, 13)); // 2600
